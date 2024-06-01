eaf465e7eb6f53eb7019740958d0f95452c49a0b
bluediamond87 <153869361+dreamdiver3@users.noreply.github.com>
2024-06-01 00:52:04 -0700

    Refactor UnitTest1 and add additional test cases
    Removed BeforeAfterTestAttribute inheritance from UnitTest1 class and shifted initialization inside its constructor. AttributeUsage annotation was commented for possible future use. Added two new test methods for multiplication and division operations.

--- UnitTest/UnitTest1.cs
+++ UnitTest/UnitTest1.cs
@@ -7,32 +7,28 @@ using Xunit.Sdk;
 namespace UnitTest2;
 using Xunit;
 
-[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
-public class UnitTest1 : BeforeAfterTestAttribute
+// [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
+public class UnitTest1 
 {
+    public string logOut = "";
+
+    public UnitTest1()
+    {
+        init();
+    }
     void init()
     {
         EchoOutFactory.OutputLogger = OutputLogger;
-
-    }
-    
-    public override void Before(MethodInfo methodUnderTest)
-    {
-        
     }
 
     private void OutputLogger(string output)
     {
         logOut = output;
     }
-
-    public string logOut = "";
     
     [Fact]
     public void Print_Add()
     {
-        init();
-        
         int val = 2.echo() + 3;
         Assert.StrictEqual("1[2+3 = 5] ", logOut); 
         
@@ -43,12 +39,31 @@ public class UnitTest1 : BeforeAfterTestAttribute
     [Fact]
     public void Print_Sub()
     {
-        init();
-        
         int val = 3.echo() - 2;
         Assert.StrictEqual("1[3-2 = 1] ", logOut); 
         
         val = 10.echo() - 5 - 4;
         Assert.StrictEqual("1[10-5 = 5] 2[5-4 = 1] ", logOut); 
     }
+
+    [Fact]
+    public void Print_Mul()
+    {
+        int val = 3.echo() * 2;
+        Assert.StrictEqual("1[3*2 = 6] ", logOut); 
+        
+        val = 10.echo() * 5 * 4;
+        Assert.StrictEqual("1[10*5 = 50] 2[50*4 = 200] ", logOut); 
+    }
+    
+    [Fact]
+    public void Print_Div()
+    {
+        int val = 6.echo() / 3;
+        Assert.StrictEqual("1[6/3 = 2] ", logOut); 
+        
+        val = 200.echo() / 4 / 10;
+        Assert.StrictEqual("1[200/4 = 50] 2[50/10 = 5] ", logOut); 
+    }
+    
 }
